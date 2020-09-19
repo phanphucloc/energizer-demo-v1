d53039691fabbd53138f4529558042f5b5ce1227
Loc Phan <lphan@openwt.com>
2020-09-19 12:13:51 +0700

    fix input number zero value

--- src/app/common/directive/input-only-number.directive.ts
+++ src/app/common/directive/input-only-number.directive.ts
@@ -11,13 +11,22 @@ export class OnlyInputNumberFormDirective {
     @HostListener('input', ['$event']) onInputChange(event) {
         const value = this.el.nativeElement.value;
         const valueFormatted = value.replace(/[^0-9]*/g, '');
-        this.control.control.patchValue(Number(valueFormatted));
-        if ((!valueFormatted || valueFormatted.trim() === '')){
-            this.control.control.patchValue(0);
-        }
+        this.control.control.patchValue(Number(valueFormatted) || '');
         if (value !== this.el.nativeElement.value) {
             event.stopPropagation();
         }
+    }
+
+    @HostListener('document:click', ['$event.target'])
+    public onClick(targetElement): void {
+      const isInsideInput: boolean = this.el.nativeElement.contains(targetElement);
+      if (isInsideInput) {}
+      else{
+        const value = this.el.nativeElement.value;
+        if (!value || value.trim() === ''){
+            this.control.control.patchValue(0);
+        }
+      }
     }
 }
 
@@ -33,11 +42,20 @@ export class OnlyInputNumberModelDirective {
         const value = this.el.nativeElement.value;
         const valueFormatted = value.replace(/[^0-9]*/g, '');
         this.model.control.patchValue(Number(valueFormatted));
-        if ((!valueFormatted || valueFormatted.trim() === '')){
-            this.model.control.patchValue(0);
-        }
         if (value !== this.el.nativeElement.value) {
             event.stopPropagation();
         }
+    }
+
+    @HostListener('document:click', ['$event.target'])
+    public onClick(targetElement): void {
+      const isInsideInput: boolean = this.el.nativeElement.contains(targetElement);
+      if (isInsideInput) {}
+      else{
+        const value = this.el.nativeElement.value;
+        if (!value || value.trim() === ''){
+            this.model.control.patchValue(0);
+        }
+      }
     }
 }
