44d5f12ab693a6cebe8f965ce1abeb5befe130cc
nhnguyen <nhnguyen@openwt.com>
2021-05-13 18:23:54 +0700

    Update excel format

--- src/app/modules/report-energizer/models/excel-report.model.ts
+++ src/app/modules/report-energizer/models/excel-report.model.ts
@@ -1,11 +1,15 @@
 export class ExcelReport {
     filedName: string;
-    data: string;
+    data2017: string;
+    data2018: string;
+    data2019: string;
     unit: string;
 
-    constructor(filedName: string, data: string, unit: string) {
+    constructor(filedName: string, data2017: string, data2018: string, data2019: string, unit: string) {
         this.filedName = filedName;
-        this.data = data;
+        this.data2017 = data2017;
+        this.data2018 = data2018;
+        this.data2019 = data2019;
         this.unit = unit;
     }
 }
